1a2c579ea5219964b218fe0ef66186f812c7a1be
monika <monikanaglak@outlook.fr>
2020-07-31 17:05:06 +0200

    hit

--- javascript/js.js
+++ javascript/js.js
@@ -186,7 +186,7 @@ function deuxcollision() {
   }
 }
 function troiscollision() {
-  if (girl.dx <= objs.x + 5 && objs.x <= girl.dx + 11) {
+  if (girl.dx <= objs.x + 5 && objs.x <= girl.dx + 15) {
     document.getElementById('js').style.display = 'block'
     score++
     baguettes.shift()
@@ -194,7 +194,7 @@ function troiscollision() {
   }
 }
 function quatrecollision() {
-  if (girl.dx <= obang.x + 5 && obang.x <= girl.dx + 10) {
+  if (girl.dx <= obang.x + 5 && obang.x <= girl.dx + 15) {
     document.getElementById('ang').style.display = 'block'
     score++
     baguettes.shift()
@@ -202,7 +202,7 @@ function quatrecollision() {
   }
 }
 function cinqcollision() {
-  if (girl.dx <= obvue.x + 10 && obvue.x <= girl.dx + 10) {
+  if (girl.dx <= obvue.x + 10 && obvue.x <= girl.dx + 15) {
     document.getElementById('vs').style.display = 'block'
     score++
     baguettes.shift()
@@ -210,7 +210,7 @@ function cinqcollision() {
   }
 }
 function sixcollision() {
-  if (girl.dx <= obps.x + 10 && obps.x <= girl.dx + 10) {
+  if (girl.dx <= obps.x + 10 && obps.x <= girl.dx + 15) {
     document.getElementById('ps').style.display = 'block'
     score++
     playsong()
